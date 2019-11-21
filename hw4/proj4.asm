ba44357d4ec6714da0114d84bcd5614c83e004c5
Robert <robus.i@hotmail.com>
2019-11-21 15:50:59 -0500

    Added hw4 skeleton

---
+++ hw4/proj4.asm
@@ -0,0 +1,36 @@
+# CSE 220 Programming Project #4
+# erase this line and type your first and last name in a comment here
+# erase this line and type your Net ID here (e.g., jmsmith) in a comment here
+# erase this line and type your SBU ID # here (e.g., 111234567) in a comment here
+
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
+
+.text
+
+compute_checksum:
+jr $ra
+
+compare_to:
+jr $ra
+
+packetize:
+jr $ra
+
+clear_queue:
+jr $ra
+
+enqueue:
+jr $ra
+
+dequeue:
+jr $ra
+
+assemble_message:
+jr $ra
+
+
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
